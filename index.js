173b83d82da078f97f6e6929c7e105ef6fac6d64
Mohammad Irfanul Islam <irfanul009@gmail.com>
2021-12-28 11:23:16 +0600

    third commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ admin.initializeApp({
       type: process.env.FIREBASE_TYPE,
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
 
